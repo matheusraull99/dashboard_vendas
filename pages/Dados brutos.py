8d2ba0ca6ae35991d7e29243533bdf318146b478
Matheus Raul <matheusraulm1@gmail.com>
2026-03-31 14:38:50 -0300

    Enhance data filtering and download functionality
    Added filters and success message for CSV download.

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -1,46 +1,77 @@
 import streamlit as st
 import requests
 import pandas as pd
-import plotly.express as px
+import time
 
-import plotly.express as px
+@st.cache_data
+def converte_csv(df):
+    return df.to_csv(index = False).encode('utf-8')
 
-st.set_page_config(layout = 'wide')
+def mensagem_sucesso():
+    sucesso = st.success('Arquivo baixado com sucesso!', icon = "✅")
+    time.sleep(5)
+    sucesso.empty()
 
-def formata_numero(valor, prefixo = ''):
-    for unidade in ['', 'mil']:
-        if valor <1000:
-            return f'{prefixo} {valor:.2f} {unidade}'
-        valor /= 1000
-    return f'{prefixo} {valor:.2f} milhões'
-
-st.title('DASHBOARD DE VENDAS :shopping_trolley:')
+st.title('DADOS BRUTOS')
 
 url = 'https://labdados.com/produtos'
+
 response = requests.get(url)
 dados = pd.DataFrame.from_dict(response.json())
 dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
 
-## Tabelas
-receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
-receita_estados = dados.drop_duplicates(subset = 'Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on = 'Local da compra', right_index = True).sort_values('Preço', ascending = False)
+with st.expander('Colunas'):
+    colunas = st.multiselect('Selecione as colunas', list(dados.columns), list(dados.columns))
 
-receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
-receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
-receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name()
+st.sidebar.title('Filtros')
+with st.sidebar.expander('Nome do produto'):
+    produtos = st.multiselect('Selecione os produtos', dados['Produto'].unique(), dados['Produto'].unique())
+with st.sidebar.expander('Categoria do produto'):
+    categoria = st.multiselect('Selecione as categorias', dados['Categoria do Produto'].unique(), dados['Categoria do Produto'].unique())
+with st.sidebar.expander('Preço do produto'):
+    preco = st.slider('Selecione o preço', 0, 5000, (0,5000))
+with st.sidebar.expander('Frete da venda'):
+    frete = st.slider('Frete', 0,250, (0,250))
+with st.sidebar.expander('Data da compra'):
+    data_compra = st.date_input('Selecione a data', (dados['Data da Compra'].min(), dados['Data da Compra'].max()))
+with st.sidebar.expander('Vendedor'):
+    vendedores = st.multiselect('Selecione os vendedores', dados['Vendedor'].unique(), dados['Vendedor'].unique())
+with st.sidebar.expander('Local da compra'):
+    local_compra = st.multiselect('Selecione o local da compra', dados['Local da compra'].unique(), dados['Local da compra'].unique())
+with st.sidebar.expander('Avaliação da compra'):
+    avaliacao = st.slider('Selecione a avaliação da compra',1,5, value = (1,5))
+with st.sidebar.expander('Tipo de pagamento'):
+    tipo_pagamento = st.multiselect('Selecione o tipo de pagamento',dados['Tipo de pagamento'].unique(), dados['Tipo de pagamento'].unique())
+with st.sidebar.expander('Quantidade de parcelas'):
+    qtd_parcelas = st.slider('Selecione a quantidade de parcelas', 1, 24, (1,24))
 
-receita_categorias = dados.groupby('Categoria do Produto')[['Preço']].sum().sort_values('Preço', ascending=False)
+query = '''
+Produto in @produtos and \
+`Categoria do Produto` in @categoria and \
+@preco[0] <= Preço <= @preco[1] and \
+@frete[0] <= Frete <= @frete[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1] and \
+Vendedor in @vendedores and \
+`Local da compra` in @local_compra and \
+@avaliacao[0]<= `Avaliação da compra` <= @avaliacao[1] and \
+`Tipo de pagamento` in @tipo_pagamento and \
+@qtd_parcelas[0] <= `Quantidade de parcelas` <= @qtd_parcelas[1]
+'''
 
-## Gráficos
-fig_mapa_receita = px.scatter_geo(receita_estados,
-                                   lat = 'lat',
-                                   lon = 'lon',
-                                   scope = 'south america',
-                                   size = 'Preço',
-                                   template = 'seaborn',
-                                   hover_name = 'Local da compra',
-                                   hover_data = {'lat':False,'lon':False},
-                                   title = 'Receita por Estado')
+dados_filtrados = dados.query(query)
+dados_filtrados = dados_filtrados[colunas]
+
+st.dataframe(dados_filtrados)
+
+st.markdown(f'A tabela possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas')
+
+st.markdown('Escreva um nome para o arquivo')
+coluna1, coluna2 = st.columns(2)
+with coluna1:
+    nome_arquivo = st.text_input('', label_visibility = 'collapsed', value = 'dados')
+    nome_arquivo += '.csv'
+with coluna2:
+    st.download_button('Fazer o download da tabela em csv', data = converte_csv(dados_filtrados), file_name = nome_arquivo, mime = 'text/csv', on_click = mensagem_sucesso)
 
 fig_receita_mensal = px.line(receita_mensal,
                                                         x = 'Mes',
